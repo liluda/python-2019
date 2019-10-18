2e94f0e1056b3672268b85e3830ff78023e6a7c4
Craig M Coleman <lil@Liluda.ksd.org>
2019-10-18 12:54:48 -0700

    AA

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
-a = int(input("Enter the hour: "))
-b = int(input("Enter the minute:"))
+import random
+random.seed()
 
-tb = ((a * 60) + b)
-tb = (tb + 15)
-ta = int(tb / 60)
-nb = tb % 60
-na = ta % 12
-if na < 1: 
-	na = 12
-print ("Hours: " ,na)  
-print ("Minutes: " ,nb)
+a = (random.randint(1, 10))
+b = (random.randint(1, 10))
+
+print ("What is: " + str(a) + " X " + str(b) + "?") 
+ans = int(input("Your answer: ")) 
+if (a * b == ans): 
+    print ("Correct!") 
+else: 
+    print ("Incorrect!")
+    
+
+
